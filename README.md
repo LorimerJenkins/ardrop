28c50c321cb3a601eaa771fe2a03f05349153025
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-02-19 16:04:55 +0000

    -

--- README.md
+++
@@ -1,2 +0,0 @@
-# ardrop
-A way to share peer-to-peer encrypted files via the Arweave protocol
---
+++ index.js
@@ -0,0 +1,15 @@
+
+// first user uploads file from UI and now we upload to arweave and encrypt or decerypt it, we return a email to both users and also where to retrive the file
+// UI params
+const file = './boys.jpg'
+const password = '1234'
+const send_to_email = 'lj@communitylabs.com'
+const send_from_email = 'lorimerjenkins1@gmail.com'
+const public_private = 'private'
+
+
+// index.js
+
+const encryptFile = require('./encrypt.js');
+
+encryptFile('my_password', './boys.jpg');
